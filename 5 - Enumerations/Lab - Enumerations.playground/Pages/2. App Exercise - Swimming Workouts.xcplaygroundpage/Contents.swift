eb9c31a4b0fd305c5bedfe8ef014c4d692091dc1
Jayx48YT <97544138+Jayx48YT@users.noreply.github.com>
2023-02-21 09:25:03 -0800

    3.5

--- 5 - Enumerations/Lab - Enumerations.playground/Pages/2. App Exercise - Swimming Workouts.xcplaygroundpage/Contents.swift
+++ 5 - Enumerations/Lab - Enumerations.playground/Pages/2. App Exercise - Swimming Workouts.xcplaygroundpage/Contents.swift
@@ -35,14 +35,17 @@ print("Daniel swam \(daniel.distance) meters in \(daniel.time) seconds while doi
 var david = SwimmingWorkout(distance: 50, time: 2, stroke: .butterfly)
 print("David swam \(david.distance) meters in \(david.time) seconds while doing the \(david.stroke).")
 
-daniel.save()
-david.save()
+
 //:  Now imagine you want to log swimming workouts separately based on the swimming stroke. You might use arrays as static variables on `SwimmingWorkout` for this. Add four static variables, `freestyleWorkouts`, `butterflyWorkouts`, `backstrokeWorkouts`, and `breaststrokeWorkouts`, to `SwimmingWorkout` above. Each should be of type `[SwimmingWorkout]` and should default to empty arrays.
 
    
 //:  Now add an instance method to `SwimmingWorkout` called `save` that takes no parameters and has no return value. This method will add its instance to the static array on `SwimmingWorkout` that corresponds to its swimming stroke. Inside `save` write a switch statement that switches on the instance's `stroke` property, and appends `self` to the proper array. Call save on the two instances of `SwimmingWorkout` that you created above, and then print the array(s) to which they should have been added to see if your `save()` method works properly.
-
-
+daniel.save()
+david.save()
+print(SwimmingWorkout.freestyle)
+print(SwimmingWorkout.breaststroke)
+print(SwimmingWorkout.butterfly)
+print(SwimmingWorkout.backstroke)
 /*:
  _Copyright © 2021 Apple Inc._
 
